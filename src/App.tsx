48a2eb4a743ce50e09a31a65d93cc5ccdf4ea31c
uzakbekov esentur <esentur_03@mail.ru>
2024-05-21 00:05:57 +0600

    level 1 done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Card from './compnents/Card';
 
 const App = () => {
 
   return (
-    <>
-     
-    </>
+    <div className="playingCards faceImages">
+      <Card rank={"k"} suit={"diams"}/>
+      <Card rank={"q"} suit={"hearts"}/>
+      <Card rank={"a"} suit={"spades"}/>
+      <Card rank={"10"} suit={"clubs"}/>
+    </div>
   )
 }
 
---
+++ src/compnents/Card.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface Props {
+    rank: string;
+    suit: 'diams' | 'hearts' | 'clubs' | 'spades';
+}
+
+const suitSymbols: {[key: string]: string} = {
+    diams: '♦',
+    hearts: '♥',
+    clubs: '♣',
+    spades: '♠',
+}
+
+const Card: React.FC<Props> = ({rank, suit}) => {
+    return(
+        <span className={`card rank-${rank} ${suit}`}>
+            <span className="rank">{rank}</span>
+            <span className="suit">{suitSymbols[suit]}</span>
+        </span>
+    )
+}
+
+export default Card;
+
+// rank={"K"} suit={"diams"}
+// ${rank} ${suit}
